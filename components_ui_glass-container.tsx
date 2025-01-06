a7257932b5ba7a8ef0b28a5f2ea8e7f1d1741a9d
SKECHIE <vomollo101@gmail.com>
2025-01-06 21:28:34 +0200

    Update components_ui_glass-container.tsx

--- components_ui_glass-container.tsx
+++ components_ui_glass-container.tsx
@@ -17,7 +17,30 @@ const GlassContainer = React.forwardRef<HTMLDivElement, GlassContainerProps>(
     )
   }
 )
+GlassContainer.displayName = "GlassContainer"import * as React from "react"
+import { cn } from "@/lib/utils"
+
+export interface GlassContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const GlassContainer = React.forwardRef<HTMLDivElement, GlassContainerProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        className={cn(
+          "bg-white/20 dark:bg-black/20 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
 GlassContainer.displayName = "GlassContainer"
+
+export { GlassContainer }
+
+
 
 export { GlassContainer }
 
